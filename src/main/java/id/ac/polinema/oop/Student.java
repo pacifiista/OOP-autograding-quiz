574778d774c2472523b8e13d5ce9e33e675b4f2a
pacifiista <naurasfh2020@gmail.com>
2026-09-24 19:07:00 +0700

    coba

--- src/main/java/id/ac/polinema/oop/Student.java
+++ src/main/java/id/ac/polinema/oop/Student.java
@@ -54,7 +54,13 @@ public class Student {
      */
     public String getPredicate() {
         // TODO: implement the predicate logic per the rules above
-        
+        if (gpa >= 3.5) {
+           return "Cum Laude";
+        } else if (gpa >= 3.0 ){
+            return "Very Satisfactory";
+        } else{
+            return "Satisfactory";
+        }
        
         
     }
